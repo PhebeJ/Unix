1ce02bdadce5d9d2de5fe5a5e36e58bb05d86f08
Phebe Joshua <feba.j7@gmail.com>
2021-07-18 11:35:21 +0530

    Create Reading Input.sh

---
+++ Reading Input.sh
@@ -0,0 +1,9 @@
+echo "enter your name"
+read a
+echo "enter your programme name"
+read b
+echo "enter your enrollment number"
+read c
+echo "your name is $a"
+echo "programme name is $b"
+echo "your enrollment number is $c"
